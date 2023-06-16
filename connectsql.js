972460742348508334b4db083b5f6bbbb9da0e13
Aly Hassan Zidan <74179544+alyzgit@users.noreply.github.com>
2023-06-17 00:47:55 +0300

    Update connectsql.js

--- connectsql.js
+++ connectsql.js
@@ -6,8 +6,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'db4free.net',
     port:3306,
-    user: 'alyz123', //Change this to your username of your account on the cloud database website that has the SQL dump
-    password:"heated2345",//Change this to your password of your account on the cloud database website that has the SQL dump
+    user: 'YourUserName', //Change this to your username of your account on the cloud database website that has the SQL dump
+    password:"YourPassword",//Change this to your password of your account on the cloud database website that has the SQL dump
     database: 'carcity123',//Change this to your database name (must match the name on the cloud database) 
     waitForConnections: true,
     connectionLimit: 10,
